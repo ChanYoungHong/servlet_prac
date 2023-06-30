a3f37e0671b519d4ab56b8dced1a9716e2bf8274
ChanYoungHong <cksdud9883@naver.com>
2023-06-30 12:34:34 +0900

    controller v5

---
+++ src/main/java/hello/servlet/web/frontcontroller/v2/ControllerV2.java
@@ -0,0 +1,8 @@
+package hello.servlet.web.frontcontroller.v2.controller;
+
+import hello.servlet.web.frontcontroller.MyView;
+
+public interface ControllerV2 {
+
+    MyView process()
+}
---
+++ src/main/java/hello/servlet/web/frontcontroller/v4/ControllerV4.java
@@ -0,0 +1,2 @@
+package hello.servlet.web.frontcontroller.v4.controller;public interface ControllerV4 {
+}
